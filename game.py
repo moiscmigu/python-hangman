925a86c0967bbd834f90f1c92c416f2534d32e69
Moises Miguel <moiesmigueledu@gmail.com>
2017-12-16 15:11:36 -0600

    version 3 with functional python

--- game.py
+++ game.py
@@ -1,43 +1,120 @@
 
 
+# THIS VERSION WILL TAKE THE EXISTING CODE AND MAKE IT FUNCTIONAL
+
+# IT WILL ALSO CHECK FOR THE TYPE OF ANSWERS THE USER ENTERS AND WILL TELL THEM THAT IT IS INVALID
+
+# WILL SHOW THE LETTERS THAT THEY HAVE USED SO FAR
+
+# WILL GIVE THE USER THE CHANCE TO PLAY AGAIN
+
+# IF THE USER QUITS THEN THE PROGRAM WILL SHOW THEIR STATS AND END THE GAME
+#     -STATS
+#         -HOW MANY TIMES THEY GUESS
+#         -WHAT WORDS THEY GOT RIGHT
+
 import random
 
-answerList = ['toy', 'plane', 'bannana', 'wedding',
-              'computer', 'christmas']
+def getRandom():
+    # will get a random word out of list of words
 
-random.shuffle(answerList)
+    answerList = ['toy', 'plane', 'bannana', 'wedding',
+                'computer']
 
-answer = list(answerList[0])
+    random.shuffle(answerList)
 
+    answer = list(answerList[0])
 
-display = []
-
-display.extend(answer)
-
-for i in range(len(answer)):
-
-    display[i] = "_"
+    return answer
 
 
+def parseGuess(guess):
 
-print(" ".join(display))
 
-correctAnswerCount = 0
+    guess = list(guess)
 
-while correctAnswerCount < len(answer):
-    print("Correct answerCount: {}/{} \n".format(correctAnswerCount, len(answer)))
-git 
-    guess = input("Please enter a letter: ").lower()
+
+    if len(guess) == 1:
+        return True
+    else:
+        return False
+
+
+def playAgain(stats):
+
+    print(stats["Tries"])
+
+    print("\n It took you {} tries to guess {}".format(stats["Tries"], stats["Words"][0]))
+
+    user = input("Would you like to play again Y/n: ").lower()
+
+    if user != 'y':
+        print("Thanks for playing. Bye")
+
+    else:
+        game()
+
+def game():
+
+    correctAnswerCount = 0
+    answer = getRandom()
+
+    usedLetters = []
+
+    display = []
+
+    display.extend(answer)
+
+
+    stats = {
+        "Words": [],
+        "Tries": 0
+    }
+
+    # CONVERT EVERY ELEMENT IN THE DISPLAY LIST TO AN UNDERSOCORE
 
     for i in range(len(answer)):
 
-        if answer[i] == guess:
-            display[i] = guess
-            correctAnswerCount += 1
+        display[i] = "_"
+
+
+    print("========== \n{}\n==========".format(" ".join(display)))
+    print("\nCorrect answerCount: {}/{}".format(correctAnswerCount, len(answer)))
 
 
 
-    print(" ".join(display))
-    print("=============================")
+    while correctAnswerCount < len(answer):
+        guess = input("Please only choose a letter: ").lower()
 
-print("Nice job, you guessed the word!")
+        stats["Tries"] += 1
+
+
+
+        # CHECK TO SEE IF THE USER ONLY ENTERED ONE LETTER
+        if parseGuess(guess):
+
+            usedLetters.append(guess)
+
+            for i in range(len(answer)):
+                if guess == answer[i]:
+                    display[i] = guess
+                    correctAnswerCount += 1
+
+        else:
+            guess = input("Please only choose ONE a letter: ").lower()
+
+        print("========== \n{}\n==========".format(" ".join(display)))
+        print("\nCorrect answerCount: {}/{} \n ".format(correctAnswerCount, len(answer)))
+        print()
+        print(" letters used: \n{}".format(" ".join(usedLetters)))
+
+    stats["Words"].append("".join(answer))
+
+    print("Well done you guessed the corrext word")
+
+
+    playAgain(stats)
+
+
+
+game()
